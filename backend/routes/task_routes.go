044d64b429a41d870fb6f70b1ea859c00e76199c
RyanFin <ryanfinlayson125@hotmail.com>
2025-06-23 11:46:11 +0100

    ammend routes

--- backend/routes/task_routes.go
+++ backend/routes/task_routes.go
@@ -9,7 +9,7 @@ import (
 func RegisterTaskRoutes(router *gin.Engine) {
 	taskGroup := router.Group("/tasks")
 	{
-		taskGroup.POST("", controllers.CreateTask)
+		taskGroup.POST("/", controllers.CreateTask)
 		taskGroup.GET("/:id", controllers.GetTask)
 		taskGroup.GET("", controllers.GetAllTasks)
 		taskGroup.PUT("/:id/status", controllers.UpdateTaskStatus)
